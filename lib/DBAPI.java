0159303e1d1270bf4c9417df188565ba97e2c310
James Beavers <jjbeaver@us.ibm.com>
2013-10-19 17:49:08 -0400

    Added DBAPI with authentication to oracle DB.  Still need to add API methods.  Also added preStart to the ODLCLI class for reinitializing the sample tables/data.

---
+++ lib/DBAPI.java
@@ -0,0 +1,81 @@
+package lib;
+import java.sql.*;
+
+public class DBAPI {
+
+    final String jdbcURL
+    = "jdbc:oracle:thin:@ora.csc.ncsu.edu:1521:orcl";
+
+    Connection conn;
+
+    public DBAPI() {
+    }
+
+
+    public boolean authDB(String user, String passwd) {
+        try {
+            // Load the driver. This creates an instance of the driver
+            // and calls the registerDriver method to make Oracle Thin
+            // driver available to clients.
+
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+
+            // Get a connection from the first driver in the
+            // DriverManager list that recognizes the URL jdbcURL
+            conn = DriverManager.getConnection(jdbcURL, user, passwd);
+        }
+        catch(Throwable err) {
+            conn = null;
+        }
+        return conn != null;
+    }
+
+    public boolean dropTables() {
+        System.out.println("Tables dropped. (not really)");
+        //insert SQL to drop all related tables
+        return true;
+    }
+
+    public boolean initTables() {
+        System.out.println("Tables initialized with sample data. (not really)");
+        //insert SQL to initialize all sample data tables
+        return true;
+    }
+
+    public boolean close(Connection conn) {
+        if(conn != null) {
+            try {
+                conn.close();
+            }
+            catch(Throwable whatever) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean close(Statement st) {
+        if(st != null) {
+            try {
+                st.close();
+            }
+            catch(Throwable whatever) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean close(ResultSet rs) {
+        if(rs != null) {
+            try {
+                rs.close();
+            }
+            catch(Throwable whatever) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
