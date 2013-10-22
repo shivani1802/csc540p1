2912dad2fe3bf5c82639a936757c7a11ffb1aa1f
mpallavi89 <pmahaja@ncsu.edu>
2013-10-22 01:54:52 -0400

    Enter, View Observations and create drop tables

--- lib/DBAPI.java
+++ lib/DBAPI.java
@@ -1,5 +1,6 @@
 package lib;
 import java.sql.*;
+import java.util.Scanner;
 
 public class DBAPI {
 
@@ -7,6 +8,8 @@ public class DBAPI {
     = "jdbc:oracle:thin:@ora.csc.ncsu.edu:1521:orcl";
 
     Connection conn;
+    Statement stmt = null;
+    ResultSet rs = null;
 
     public DBAPI() {
     }
@@ -23,6 +26,7 @@ public class DBAPI {
             // Get a connection from the first driver in the
             // DriverManager list that recognizes the URL jdbcURL
             conn = DriverManager.getConnection(jdbcURL, user, passwd);
+            stmt = conn.createStatement();
         }
         catch(Throwable err) {
             conn = null;
@@ -32,15 +36,173 @@ public class DBAPI {
 
     public boolean dropTables() {
         System.out.println("Tables dropped. (not really)");
+        try
+        {
+       stmt.executeUpdate("drop table Makes_Observation");
+       stmt.executeUpdate("drop table Observations");
+      stmt.executeUpdate("drop table Has_Illness");
+      stmt.executeUpdate("drop table patient_info");
+       stmt.executeUpdate("drop table Observation_Type");
+        }
+        catch(Throwable err) {
+            conn = null;
+            System.out.println("Not dropped table" + err);
+        }
         //insert SQL to drop all related tables
         return true;
     }
 
     public boolean initTables() {
         System.out.println("Tables initialized with sample data. (not really)");
+        try
+        {
+       // stmt = conn.createStatement();
+       stmt.executeUpdate("CREATE TABLE Patient_Info " +
+ 			"(Patient_NAME VARCHAR(32), Address varchar (100), Age integer,Sex varchar(6),Patient_Id varchar (10), Password varchar(10), PublicStatus char(2), primary key (Patient_Id))" );
+     //  System.out.println("inserted table Patient");
+       stmt.executeUpdate("INSERT INTO Patient_Info " +
+			   "VALUES ('Gary George', '2806 Confer Drive Raleigh NC 27606', 25 , 'Male','ggeorge','geo123','Y')");
+       //System.out.println("inserted value Patient");
+       stmt.executeUpdate("create table Observation_Type"+
+"(Illness varchar (10),Type varchar (40), Category varchar(40), AdditionalInfo varchar (200), primary key (Illness, Type))");
+       //System.out.println("created table oBS");
+       stmt.executeUpdate("INSERT INTO Observation_Type (Illness, Type, Category, AdditionalInfo)" +
+			   "VALUES ('HIV','Temprature','Physiological','Amount in Fahrenheit')");
+      
+      //System.out.println("inserted table oBS");
+      
+       stmt.executeUpdate("INSERT INTO Observation_Type (Illness, Type, Category, AdditionalInfo)" +
+			   "VALUES ('General', 'Diet','Behavioral','What was consumed and Amount in sevings')");
+       stmt.executeUpdate("INSERT INTO Observation_Type (Illness, Type, Category, AdditionalInfo)" +
+			   "VALUES ('General', 'Weight','Physiological','Amount in Pounds')");
+      
+        stmt.executeUpdate("create table Has_Illness"+
+        		"(Patient_Id varchar(10), Illness varchar (10),  foreign key(Patient_Id) references Patient_Info(Patient_Id))");
+        
+        stmt.executeUpdate("INSERT INTO Has_Illness (Patient_Id, Illness)" +
+ 			   "VALUES ('ggeorge', 'HIV')");
+       
+        stmt.executeUpdate("create table Observations"+
+"(OId varchar(5), Type varchar(40), Date_of_observation varchar(20), time_of_observation varchar(20), AdditionalInfo varchar(100), primary key (OId))");
+      //  System.out.println("not inserted table observations new");
+       // stmt.executeUpdate("Insert into Observations (OId, Type, Date_of_observtion,time_of_observation,AdditionalInfo)"+
+//"values ('O1', 'Diet', to_date('01-01-2011','dd-mm-yyyy'),to_date('18:12:02', 'HH24:MI:SS'),'What was consumed and amount in servings: egg 1, orange ½, toast 1, margarine 1')");
+        stmt.executeUpdate("Insert into Observations (OId, Type, Date_of_observation,time_of_observation,AdditionalInfo)"+
+        		"values ('O1', 'Diet','01-01-2011','18:12:02','What was consumed and amount in servings: egg 1, orange ½, toast 1, margarine 1')");
+     
+       
+         stmt.executeUpdate("create table Makes_Observation" +
+"(pId varchar(10), OId varchar(5), Date_of_record varchar(20), time_of_record varchar(20), foreign key (pId) references Patient_Info(Patient_Id), foreign key(OId) references Observations)");
+        // System.out.println("created table makesobs");
+         stmt.executeUpdate("Insert into Makes_Observation (pId, OId, Date_of_record ,time_of_record)"+
+        		"values ('ggeorge', 'O1', '01-01-2011','18:12:02')");
+        }
+
+        catch(Throwable err) {
+            conn = null;
+            System.out.println("Not inserted" + err);
+        }
         //insert SQL to initialize all sample data tables
         return true;
     }
+    
+    public void observationMenu(String patientId)
+    {
+    	int i=1;
+    	        Scanner sc= new Scanner(System.in);
+    	
+      	try
+    	{
+    		
+    		
+    	rs = stmt.executeQuery("select type from Observation_Type where Illness ='General' or Illness in ( SELECT Illness FROM Has_Illness where Patient_Id= '"+ patientId +"')");
+    //	System.out.println(patientId);
+    	// String typeObv1 = rs.getString("type");
+		  //  System.out.println(i+". " + typeObv1);
+    	while (rs.next()) {
+		    String typeObv = rs.getString("type");
+		    System.out.println(i+". " + typeObv);
+		    i++;
+		}
+    	}
+      	 catch(Throwable err) {
+             conn = null;
+             System.out.println("querry nt executed" + err);
+             
+             }
+    }
+      	public void enterObservation( String patientId,String Obs_Type,String obsDate,String obsTime)
+      	{
+      	//	System.out.println("hello");
+      		int j=0;
+      		String Obs_Data=null;
+      		Scanner sc= new Scanner(System.in);
+          try
+       {
+    	rs = stmt.executeQuery("select AdditionalInfo from Observation_Type where Type ='"+Obs_Type+"' and Illness ='General' or Illness in ( SELECT Illness FROM Has_Illness where Patient_Id= '"+ patientId +"')");
+        	 // rs = stmt.executeQuery("select AdditionalInfo from Observation_Type where Type ='Diet' and Illness ='General' or Illness in ( SELECT Illness FROM Has_Illness where Patient_Id= 'ggeorge')");
+    	while (rs.next()) {
+		    String AdditionalInfo = rs.getString("AdditionalInfo");
+		  String [] data= AdditionalInfo.split(",");
+		  
+		  while(j!=data.length)
+		  {
+			  System.out.println("Enter "+ data[j]);
+			  Obs_Data+= ""+ sc.next();
+			  j++;			  
+		  }
+		  stmt.executeUpdate("Insert into Observations "+
+"values ('O2','"+Obs_Type+"','"+ obsDate +"','"+ obsTime + "','"+ Obs_Data +"')");
+		 // System.out.println(patientId);
+		  
+		    // do we need a trigger instead of insert i makes here ??
+		  
+		 //If no trigger how to enter corresponding oid..that would be an issue 
+		  
+		  stmt.executeUpdate("Insert into Makes_Observation (pId, OId, Date_of_record ,time_of_record)"+
+	        		"values ('"+patientId+"', 'O1', '01-01-2011','18:12:02')"); // here date and time are supposed to be system date and time as i trigger below but for the time being we have this
+		  
+		  /*
+		   create or replace Trigger MakesObservationInsert
+		   After Inser on Observations
+		   Referencing new row as NewRow
+		   insert into Makes_Observaton (pId, OId, Date_of_record ,time_of_record)
+		   values (patient_Id, NewRow.OId, TO_CHAR(SYSDATE,'dd-Mon-yyy'),TO_CHAR(SYSDATE,' hh:mi:ss PM'))
+		   */
+		}
+    	
+    	}
+    	  catch(Throwable err) {
+              conn = null;
+              System.out.println("querry nt executed" + err);
+              
+              }
+    	 }
+
+    	public void displayObservation(String patientId)
+      	{
+      		int i=0;
+      		
+          try
+       {
+    	rs = stmt.executeQuery("select * from Observations o,makes_observation m where o.oid=m.oid and m.pid='"+patientId+"'");  
+    	
+    	while (rs.next()) {
+		    
+		    System.out.println(i+". " + rs.getString("type"));
+		    System.out.println(rs.getString("Oid"));
+		    System.out.println(rs.getString("date_of_observation"));
+		    System.out.println(rs.getString("time_of_observation"));
+		    System.out.println(rs.getString("AdditionalInfo"));
+		    i++;
+		}    	
+    	}
+    	  catch(Throwable err) {
+              conn = null;
+              System.out.println("querry nt executed" + err);
+              
+              }
+          }
 
     public boolean close(Connection conn) {
         if(conn != null) {
@@ -78,4 +240,3 @@ public class DBAPI {
         return true;
     }
 }
-
--- ui/ODLCLI.java
+++ ui/ODLCLI.java
@@ -1,6 +1,8 @@
 package ui;
 import java.util.Scanner;
-import lib.*;
+import DBAPI.DBAPI;
+//import lib.*;
+
 
 /**
  * Description here.
@@ -14,11 +16,13 @@ public class ODLCLI
     {
         preStart();
         startMenu();
+        
     }
 
     public static void main(String [] args)
     {
         new ODLCLI();
+        
     }
 
     public void preStart() {
@@ -26,9 +30,11 @@ public class ODLCLI
                             "username and password.\n");
 
         Scanner sc = new Scanner(System.in);
-        System.out.print("Username (ie \"jsmith\"): ");
+        System.out.println("Username (ie \"jsmith\"): ");
         String user = sc.next();
-        String passwd = new String(System.console().readPassword("Password: "));
+        System.out.print("Password: ");
+        String passwd = sc.next();
+       //p String passwd = new String(System.console().readPassword("Password: "));
         
         api = new DBAPI();
         if (!api.authDB(user, passwd)) {
@@ -36,7 +42,9 @@ public class ODLCLI
             preStart();
         }
         promptReinit();
-    }
+        obsevationMenuPatient();
+       
+    }    
 
     public void promptReinit() {
         System.out.print("\nWould you like to delete any existing tables and " + 
@@ -107,7 +115,8 @@ public class ODLCLI
                 login();
         }*/
     }
-
+    
+   
     public void createUser()
     {
         System.out.println("Create user screen not yet implemented, returning to start menu.");
@@ -118,4 +127,67 @@ public class ODLCLI
         System.out.println(msg + "\n");
         System.exit(status);
     }
+    
+    public void obsevationMenuPatient()
+    {
+while(true)
+{
+        Scanner in = new Scanner(System.in);
+        System.out.println("\n\n\n\n===== Observations of Dailing Living -- Observation Menu =====\n");
+        System.out.println("Available Options:");
+        System.out.println(" 1.  Enter Observations");
+        System.out.println(" 2.  View Observations");
+        System.out.println(" 3.  Add a new Observation Type");
+        System.out.println(" 4.  View my Alerts");
+        System.out.println(" 5  Manage HealthFriends");
+        System.out.println(" 6.  Back");
+        System.out.print("\nInput: ");
+        String input = in.next();
+        switch (input) {
+            case "1":
+                recordObservation();
+                break;
+            case "2":
+                viewObservation("ggeorge");
+                break;
+            case "3":
+            	addObservationType();
+                break;
+            case "4":
+                createUser();//write alert function name here
+                break;
+            case "5":
+                login();//manage health friends
+                break;
+            case "6":
+                startMenu();;
+            default:
+                System.out.println("Invalid input. Please Try again.");
+                obsevationMenuPatient();
+        }
+       }
     }
+    public void recordObservation()
+    {
+    	String obsType,obsDate,obsTime;
+    	Scanner sc = new Scanner(System.in);
+    	System.out.println("Enter observations for the following available types based on your Illness :");
+    	api.observationMenu("ggeorge");
+    	System.out.print("Enter your type of Observation : ");
+    	obsType= sc.next();
+    	System.out.println(obsType +":\nEnter :");
+    	System.out.println("Enter Date of Observation in mm/dd/yyyy format :");
+    	obsDate= sc.next();
+    	System.out.println("Enter Time of Observation in HH:mm:ss format :");
+    	obsTime= sc.next();
+    	api.enterObservation("ggeorge",obsType,obsDate,obsTime);
+    	
+    }
+    public void viewObservation(String patientId)
+    {
+    	api.displayObservation(patientId);
+    }
+    public void addObservationType()
+    {
+    }
+}
